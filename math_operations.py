c884d1c4ad62a21793a87bfacb859c303948a0f2
hyuqing11 <yuiqinghuang2018@gmail.com>
2024-07-31 11:11:18 -0500

    first commit

---
+++ math_operations.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    print(
+        "this function is used to the numbers a and b and add them together. Then it will return the results"
+    )
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+print(add(3, 4))
